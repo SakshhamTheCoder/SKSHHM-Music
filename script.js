6a728c990b872ffb131e021d7e6d85d35741ef89
SakshhamTheCoder <sakshhamtg@gmail.com>
2023-11-16 22:34:20 +0530

    LFS fix

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const trackList = {
-    "Feel Good" : ["assets/images/album-arts/Feel_Good.png", "assets/music/SKSHHM - Feel Good.wav"],
+    "Feel Good" : ["assets/images/album-arts/Feel_Good.png", "https://media.githubusercontent.com/media/SakshhamTheCoder/SKSHHM-Music/main/assets/music/SKSHHM%20-%20Feel%20Good.wav"],
     "Something Just Like This (SKSHHM Remix)" : ["assets/images/album-arts/Something_Just_Like_This_REMIX.png", "assets/music/sjltremix2.mp3"],
     "Tie Me Down (SKSHHM Remix)" : ["assets/images/album-arts/Tie_Me_Down_REMIX.png", "assets/music/Gryffin - Tie Me Down (ft. Elley Duhé) (SKSHHM Remix).mp3"],
     "Used To Love (SKSHHM Remix)" : ["assets/images/album-arts/Used_To_Love_REMIX.png", "assets/music/anothergoodmelody2.0_2.mp3"],
     "Andromeda" : ["assets/images/album-arts/Andromeda.png", "assets/music/SKSHHM - Andromeda.mp3"],
     "Lyra" : ["assets/images/album-arts/Lyra.png", "assets/music/SKSHHM - Lyra.mp3"],
     "Orion" : ["assets/images/album-arts/Orion.png", "assets/music/SKSHHM - Orion.mp3"],
-    "Heartbreak" : ["assets/images/album-arts/Heartbreak.png", "assets/music/SKSHHM - Heartbreak (Future Bounce).wav"],
+    "Heartbreak" : ["assets/images/album-arts/Heartbreak.png", "https://media.githubusercontent.com/media/SakshhamTheCoder/SKSHHM-Music/main/assets/music/SKSHHM%20-%20Heartbreak%20(Future%20Bounce).wav"],
     "Dreams" : ["assets/images/album-arts/Dreams.png", "assets/music/Dreams.mp3"],
 };
 
